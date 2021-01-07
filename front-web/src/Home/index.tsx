9f0249a99329931167cb6e6af12525599c82dbaf
pedro <pedrohwbauer@gmail.com>
2021-01-06 21:49:31 -0300

    Add Home Page

---
+++ front-web/src/Home/index.tsx
@@ -0,0 +1,30 @@
+import './styles.css'
+import { ReactComponent as MainImage } from './main.svg'
+
+function Home() {
+  return (
+    <div className="home-container">
+        <div className="home-content">
+            <div className="home-actions">
+                <h1 className="home-title">
+                    Place your order <br/>
+                    and we will <br/>
+                    deliver it to you!
+                </h1>
+                <h3 className="home-subtitle">
+                    Place your order and in a few minutes we <br/>
+                    will bring it to your door
+                </h3>
+                <a href="" className="home-btn-order">
+                    PLACE ORDER
+                </a>
+            </div>
+            <div className="home-image">
+                <MainImage />
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Home
