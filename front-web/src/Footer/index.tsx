8894876d5f40b89af9516c6f62b694ccefbf5e22
pedro <pedrohwbauer@gmail.com>
2021-01-07 01:57:20 -0300

    Add footer

---
+++ front-web/src/Footer/index.tsx
@@ -0,0 +1,24 @@
+import './styles.css'
+import { ReactComponent as YouTubeIcon } from './youtube.svg'
+import { ReactComponent as LinkedinIcon } from './linkedin.svg'
+import { ReactComponent as InstagramIcon } from './instagram.svg'
+
+function Footer() {
+  return (
+    <footer className="main-footer">
+      <div className="footer-icons">
+        <a href="#" target="_new">
+          <YouTubeIcon />
+        </a>
+        <a href="#" target="_new">
+          <LinkedinIcon />
+        </a>
+        <a href="" target="_new">
+          <InstagramIcon />
+        </a>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
