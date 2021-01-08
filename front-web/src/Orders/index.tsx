58c3e7a340188696376acf596d70a72038797e09
pedro <pedrohwbauer@gmail.com>
2021-01-07 21:04:30 -0300

    Fetch products from backend and show on the frontend

--- front-web/src/Orders/index.tsx
+++ front-web/src/Orders/index.tsx
@@ -1,10 +1,25 @@
+import { useEffect, useState } from 'react'
+import { fetchProducts } from '../api'
+import ProdcutsList from './ProductsList'
 import StepsHeader from './StepsHeader'
 import './styles.css'
+import { Product } from './types'
 
 function Orders() {
+  const [products, setProducts] = useState<Product[]>([])
+
+  useEffect(() => {
+    fetchProducts()
+      .then(response => setProducts(response.data))
+      .catch(error => console.log(error))
+  }, [])
+
+  console.log(products)
+
   return (
     <div className="orders-container">
       <StepsHeader />
+      <ProdcutsList products={products}/>
     </div>
   )
 }
